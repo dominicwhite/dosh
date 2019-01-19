76449f8706d649c141af008d93c59a39aa53652a
Dominic White <dewhite4@gmail.com>
2019-01-18 21:24:29 -0500

    Add 'exit' builtin command'

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void check_builtin(char *token) {
+    printf("Checking against builtin commands\n");
+    const char *builtin = "exit";
+    if (strncmp(token, builtin, 4) == 0) {
+        printf("Goodbye.\n");
+        exit(0);
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Welcome to dosh, the Dominic shell\n");
     while (1 == 1){
-        printf("wish> ");
+        printf("dosh> ");
         char *command = malloc(3 * sizeof(char));
         *command = NULL;
         size_t n = 3;
@@ -12,6 +22,15 @@ int main() {
         printf("You typed: %s\n", command);
         printf("The line has length: %zu\n", n2);
         printf("The buffer has size: %zu\n", n);
+        const char delim = ' ';
+        char *token = strtok(command, &delim);
+        int token_counter = 0;
+        while (token != NULL){
+            token_counter++;
+            if (token_counter == 1) check_builtin(token);
+            printf("Token: %s\n", token);
+            token = strtok(NULL, &delim);
+        }
     }
     return 0;
 }
